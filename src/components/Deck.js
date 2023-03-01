6d4275660f02476771b781c8d51a006c8958ba70
MarianaKely <marianakely.r@gmail.com>
2023-03-01 09:38:04 -0300

    deck questions and answers implemented

---
+++ src/components/Deck.js
@@ -0,0 +1,11 @@
+
+const cards = [
+	{ question: "Antes de Dumbledore, quem era o Diretor de Hogwarts?", answer: "Armando Dippet" },
+	{ question: "Qual é o nome do vilarejo próximo a Hogwarts", answer: "Hogsmeade" },
+	{ question: "Qual feitiço fornece proteção contra Dementadores?", answer: "Expecto Patronum" },
+	{ question: "De qual dos fantasmas de Hogwarts Pirraça tem medo?", answer: "O Barão Sangrento" },
+	{ question: "Qual é o encantamento para o Feitiço da Memória?", answer: "Obliviate" },
+	{ question: "O que é um animago?", answer: "Uma pessoa com forma animal" },
+	{ question: "Qual é o termo para um objeto feito com um fragmento da alma de um mago?", answer: "Horcrux" },
+	{ question: "Qual alquimista esteve na ativa durante a era medieval?", answer: "Nicholas Flamel" }
+]
